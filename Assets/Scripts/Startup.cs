0ab9e8e294a5b9657bf90cae5d911176ea9beb97
Tokki Mob <youngrim2712@naver.com>
2015-09-28 16:51:32 +0900

    [버그수정] 화면 크기 650x900으로 고정되지 않는 문제 수정.
    - 그런데 화면 크기는 맞췄는데 여백이 남아보이는 건 왜 이러는 건지 이유를 모르겠다. resolution도 다른 걸로 뜨던데. 화면 사이즈를 바꿔야 하나?
    - 버그 수정을 위해 Title scene 임의로 추가.
    - 중간에 MobSpawn_js.js를 잘못 날려서 깃헙 보고 다시 추가 ㅠㅠ

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Screen.SetResolution(650, 900, false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("space"))
+        {
+            Application.LoadLevel( "Stage" );
+        }
+    }
+}
