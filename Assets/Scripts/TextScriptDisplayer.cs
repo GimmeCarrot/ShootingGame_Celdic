92d9757ffa197c851c259d1f9054afdc3d0f0cbd
LemonTeaDev <jinuxc@gmail.com>
2015-10-07 00:54:02 +0900

    Add Dev Scene, Font, JSON library, working on TextScriptDisplayer

--- Assets/Scripts/TextScriptDisplayer.cs
+++ Assets/Scripts/TextScriptDisplayer.cs
@@ -1,28 +1,33 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class TextScriptDisplayer : MonoBehaviour {
 
-	public SpriteRenderer textBoxRender;
-	public GameObject bgObject;
+	public CanvasRenderer textFrame;
+    public CanvasRenderer charNameBox;
+    public CanvasRenderer textBox;
     public string jsonInput;
+
+    private List<DisplayUnit> displayList = new List<DisplayUnit>();
 
     private class DisplayUnit
     {
+        private int displayMs;
 		private int charIdx;
 		private string text;
 
-		public DisplayUnit(int _charIdx, string _text)
+        public DisplayUnit(float _displayMs, float _charIdx, string _text)
 		{
-			charIdx = _charIdx;
+            displayMs = (int)_displayMs;
+            charIdx = (int)_charIdx;
 			text = _text;
 		}
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        JSONObject textJson = new JSONObject(jsonInput);
+        ParseJSON(textJson);
 	}
 
 	// Update is called once per frame
@@ -39,5 +44,37 @@ public class TextScriptDisplayer : MonoBehaviour {
     void OnGUI()
     {
 		
+    }
+
+    private bool IsValidObject(JSONObject obj)
+    {
+        if (obj.list.Count != 3)
+            return false;
+
+        return
+            obj.keys[0] == "time" && obj.keys[1] == "char" && obj.keys[2] == "text" &&
+            obj.list[0].type == JSONObject.Type.NUMBER &&
+            obj.list[1].type == JSONObject.Type.NUMBER &&
+            obj.list[2].type == JSONObject.Type.STRING;
+    }
+
+    private void ParseJSON(JSONObject obj)
+    {
+        switch (obj.type)
+        {
+            case JSONObject.Type.OBJECT:
+                if (IsValidObject(obj))
+                {
+                    DisplayUnit unit = new DisplayUnit(obj.list[0].n, obj.list[1].n, obj.list[2].str);
+                    displayList.Add(unit);
+                }
+                break;
+            case JSONObject.Type.ARRAY:
+                foreach (JSONObject subObj in obj.list)
+                {
+                    ParseJSON(subObj);
+                }
+                break;
+        }
     }
 }
