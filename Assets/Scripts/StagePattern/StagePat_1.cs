d7b684e2f48f5ce13d54f24acc0c8c072f9cd0df
Tokki Mob <youngrim2712@naver.com>
2015-10-23 20:32:48 +0900

    [기능추가] 보스몹 패턴 짜기 밑작업을 위한 보스몹 소환 && 스테이지 페이즈 구분 추가.
    - 몹 같은 걸 array로 관리하고 싶은데 배가 고파서 아직 못 짬....orz
    - 지금은 그냥 2초 지나면 보스몹 소환됨.

---
+++ Assets/Scripts/StagePattern/StagePat_1.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StagePat_1 : MonoBehaviour
+{
+
+    enum Phase
+    {
+        PHASE_NORMAL,
+        PHASE_BOSS,
+        PHASE_CLEAR,
+    };
+
+    public GameObject m_enemy1;
+    public GameObject m_boss;
+
+    public float m_tSpawnEnemy1;
+
+    private Phase m_ePhase;
+    private int m_nMobAlive;
+    private float m_tUpdateTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (m_enemy1 == null || m_boss == null)
+            Debug.LogError("No GameObject");
+
+        if (m_tSpawnEnemy1 <= 0)
+            m_tSpawnEnemy1 = 2;
+
+        m_ePhase = Phase.PHASE_NORMAL;
+        m_nMobAlive = 0;                  // MobMan에서 array든 뭐든 생성한 애들 관리를 해야 하는데... 일단은 이렇게 구실을.
+        m_tUpdateTime = Time.time;
+
+        //CallEnemy();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (m_ePhase)
+        {
+            case Phase.PHASE_NORMAL:
+                if (m_tUpdateTime + m_tSpawnEnemy1 < Time.time)
+                {
+                    if (m_nMobAlive == 0)
+                    {
+                        m_ePhase = Phase.PHASE_BOSS;
+                        CallBoss();
+                    }
+                }
+                break;
+            case Phase.PHASE_BOSS:
+                break;
+            case Phase.PHASE_CLEAR:
+                break;
+            default:
+                Debug.LogError("No Phase Info");
+                break;
+        }
+    }
+
+    void CallEnemy()
+    {
+        float x1 = -3.0f;
+        float x2 = 3.0f;
+
+        Instantiate(m_enemy1, new Vector3(Random.Range(x1, x2), transform.position.y, 0), Quaternion.identity);
+        m_nMobAlive++;
+    }
+
+    void CallBoss()
+    {
+        float fBossY = 1;
+        Instantiate(m_boss, new Vector3(0, fBossY, 0), Quaternion.identity);
+
+        m_nMobAlive = 1;
+    }
+}
