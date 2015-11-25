494c37d6558db68e59f28c0d534ae6192ce27abd
Tokki Mob <youngrim2712@naver.com>
2015-11-25 23:29:37 +0900

    [기능추가] HP, CP바 추가
    - HP, CP바 더미 이미지 추가
    - 플레이어의 HP, CP 스탯에 따라 게이지 증감 추가
    - 보스몹에게 데미지 받을 때 플레이어의 CP 증가 기능 추가

---
+++ Assets/Scripts/EnemyStat/BossStat1.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossStat1 : MonoBehaviour {
+
+    enum NUMBERS
+    {
+        MAX_HP = 99999,
+    };
+
+    public int m_nHP;
+    public int m_nEP;
+    public int m_nCP;
+
+    private GameObject m_playerObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (m_nHP <= 0)
+        {
+            m_nHP = (int)NUMBERS.MAX_HP;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( m_playerObj == null )
+        {
+            m_playerObj = GameObject.FindGameObjectWithTag("Player");
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        string sColliderTag = coll.gameObject.tag;
+
+        if (sColliderTag == "PlayerAtk")
+        {
+            // Increase the player's CP
+            if( m_playerObj )
+                m_playerObj.SendMessage("IncCP", 1);
+
+            m_nHP -= 1;
+            if (m_nHP <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            Destroy(coll.gameObject);
+        }
+        else if (sColliderTag == "Player")
+        {
+            // Increase the player's CP
+            if( m_playerObj)
+                m_playerObj.SendMessage("IncCP", 2);
+
+            //Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/player_stat.cs
+++ Assets/Scripts/Player/player_stat.cs
@@ -1,24 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
 
-public class player_stat : MonoBehaviour {
+public class player_stat : MonoBehaviour
+{
 
-    public float fHP;
-    public float fEP;
-    public float fCP;
+    enum NUMBERS
+    {
+        MAX_HP = 5,
+        MAX_CP = 200,
+        MID_CP = 100,
+    };
 
-	// Use this for initialization
-	void Start () {
-        if (fHP <= 0)
+    public int m_nHP;
+    public int m_nCP;
+
+    public Image m_Bar_HPRed;
+    public Image m_Bar_CPGreen;
+    public Image m_Bar_CPGreen2;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (m_nHP <= 0)
         {
-            fHP = 5;
+            m_nHP = (int)NUMBERS.MAX_HP;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        m_nCP = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Bar_HPRed.fillAmount = (float)m_nHP / (float)NUMBERS.MAX_HP;
+
+        if ( m_nCP <= (int)NUMBERS.MID_CP )
+        {
+            m_Bar_CPGreen.fillAmount = (float)m_nCP / (float)NUMBERS.MID_CP;
+            m_Bar_CPGreen2.fillAmount = 0;
+        }
+        else
+        {
+            m_Bar_CPGreen.fillAmount = 1;
+            m_Bar_CPGreen2.fillAmount = (float)(m_nCP - (int)NUMBERS.MID_CP) / (float)NUMBERS.MID_CP;
+        }
+        
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
@@ -26,11 +53,22 @@ public class player_stat : MonoBehaviour {
 
         if (sColliderTag == "Mob")
         {
-            fHP -= 1;
-            if (fHP <= 0)
+            m_nHP -= 1;
+            if (m_nHP <= 0)
             {
                 Destroy(gameObject);
             }
         }
+    }
+
+    // HP, CP 증감 함수. 물론 마이너스값 들어오면 감소한다.
+    void IncHP(int nInc)
+    {
+        m_nHP += nInc;
+    }
+
+    void IncCP(int nInc)
+    {
+        m_nCP += nInc;
     }
 }
