4cad050957d3ea8091b30b77388e05db2c0ab184
Tokki Mob <youngrim2712@naver.com>
2016-03-20 20:58:49 +0900

    [기능추가] Menu Scene에서 Q, W키 입력시 메뉴 좌우로 변경되는 기믹 추가.
    - 좌측 메뉴 - 중앙 메뉴(스테이지 셀렉트) - 우측 메뉴(환경설정)
    Q, W 키로 좌우로 이동 가능.
    - 원래는 메뉴 이동시 카메라 애니메이션을 줘서 진짜 이동하는 느낌으로 하고 싶었는데 아직 잘 안 되서, 일단 다음 커밋(?)으로 미룬다.

---
+++ Assets/Scripts/System/Menu_ArrowScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_ArrowScript : MonoBehaviour
+{
+
+    enum PAGES
+    {
+        PAGE_FIE = -1,
+        PAGE_ELIOT = 0,
+        PAGE_MACHIAS = 1,
+    };
+
+    private PAGES m_nPresentPage;
+    private Vector3 m_vVelocity = Vector3.zero;
+    private float m_fMenuMoveSpeed = 0.3F;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_nPresentPage = PAGES.PAGE_ELIOT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 정확히 포지셔닝을 하고 싶은데 일단 이미지도 더미고... 해서 대충 지금 더미에 맞춰 잡는다.
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (m_nPresentPage > PAGES.PAGE_FIE)
+            {
+                m_nPresentPage--;
+                transform.Translate(-7, 0, 0);
+                //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(-7, 0, 0), ref m_vVelocity, m_fMenuMoveSpeed);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (m_nPresentPage < PAGES.PAGE_MACHIAS)
+            {
+                m_nPresentPage++;
+                transform.Translate(7, 0, 0);
+            }
+        }
+    }
+}
